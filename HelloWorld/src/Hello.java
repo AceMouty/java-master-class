8b58fc240db3326c4c9205555db10a33a547786d
kyle Guerrero <kyleguerrero85@gmail.com>
2020-04-23 13:14:49 -0500

    feat: created hello world class
    created a basic class that prints out hello world to the console

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,19 @@
+
+/*
+
+ the "public" keyword is an access modifier, "public" gives us full access.
+ An access modifier allows us to define the scope or how other files can access code in this file
+ to define a class we need an optional access modifier and also the "class" keyword
+
+*/
+public class Hello {
+
+/*
+    public: access modifier
+    static: we need to have static for java to find our method to run the code we have in the main fn
+    void: indicates that the method will not return anything.
+ */
+    public static void main(String[] args){
+        System.out.println("Hello world!!");
+    }
+}
