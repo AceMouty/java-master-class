50c1d51e1ca2bf39e84308842a977cc528ebd258
kyle Guerrero <kyleguerrero85@gmail.com>
2020-05-27 14:45:06 -0500

    feat: finished challenges
    completed challenges DayOfTheWeek: using a switch statement display
    the day of the week based on a number passed from 0 - 6.
    
    NumberIsPrime: display if a number is a prime number or not
    based on a range of numbers generated with a for loop.
    
    SumThreeAndFive: sum up numbers that are divisable between three and five
    from 1 to 1000 and break the loop once you have found 5 numbers that
    meet the condition.

--- 02.ControlFlow/00.Exercises/1.DayOfTheWeek/src/Main.java
+++ 02.ControlFlow/00.Exercises/1.DayOfTheWeek/src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int dayOfTheWeek = 5;
+        printDayOfTheWeek(dayOfTheWeek);
+    }
+
+    public static void printDayOfTheWeek(int day){
+        switch (day){
+            case 0:
+                System.out.println("Today is Sunday");
+                break;
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+            case 4:
+                System.out.println("Today is Thursday");
+                break;
+            case 5:
+                System.out.println("Today is Friday");
+                break;
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("That is not a day in the week....");
+                break;
+        }
+    }
 }
--- 02.ControlFlow/00.Exercises/2.NumberIsPrime/src/Main.java
+++ 02.ControlFlow/00.Exercises/2.NumberIsPrime/src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        // create a for loop and determine if a number is prime or not
+
+        for (int i = 0; i < 300; i++){
+            if (isPrime(i)) {
+                System.out.println(i + " is a prime number");
+            }
+        }
+    }
+
+    // helper function to see if a number is prime or not
+    public static boolean isPrime(int number) {
+        if (number ==1){
+            return false;
+        }
+
+        for (int i = 2; i <= number/2; i++){
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
