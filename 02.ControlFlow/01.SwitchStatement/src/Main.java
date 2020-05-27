be91eaa3a7cf02f8788b09370955902a562ab0e3
kyle Guerrero <kyleguerrero85@gmail.com>
2020-05-27 14:45:53 -0500

    feat: completed switch and for loops
    completed notes and work for switch statements and for loops

--- 02.ControlFlow/01.SwitchStatement/src/Main.java
+++ 02.ControlFlow/01.SwitchStatement/src/Main.java
@@ -1,4 +1,72 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args)
+    {
+        int value = 1;
+//        if (value == 1)
+//        {
+//            System.out.println("The value equals 1");
+//        }
+//        else if (value == 2)
+//        {
+//            System.out.println("The value equals 2");
+//        }
+//        else if (value == 3)
+//        {
+//            System.out.println("The value equals 3");
+//        }
+
+        // convert the if into a switch statement
+        switch (value)
+        {
+            case 1:
+                System.out.println("The value is equal is one");
+                break;
+            case 2:
+                System.out.println("The value is equal to two");
+                break;
+            // we can also stack cases so that we can have a code block run if any of the cases match
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("The value is equal to three, four or five");
+                System.out.println("The value is " + value);
+                break;
+            default:
+                System.out.println("Value not supported");
+                break;
+        }
+
+        /*
+            Create a new switch statement using char instead of int
+            create a new char variable
+            create a swi9tch statement testing for
+            A, B, C, D, or E
+            display a message if any of these are found and then break
+            add a default which displays a message saying not found
+         */
+
+         char ranChar = 'A';
+
+         switch (ranChar)
+         {
+             case 'A':
+                 System.out.println("The random char is A");
+                 break;
+             case 'B':
+                 System.out.println("The random char is B");
+                 break;
+             case 'C':
+                 System.out.println("The random char is C");
+                 break;
+             case 'D':
+                 System.out.println("Then random char is D");
+                 break;
+             case 'E':
+                 System.out.println("The random char is E");
+                 break;
+             default:
+                 System.out.println("Char passed is not supported");
+                 break;
+         }
+    }
 }
--- 02.ControlFlow/02.ForLoop/src/Main.java
+++ 02.ControlFlow/02.ForLoop/src/Main.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        // manually calculating an interest rate...
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.00, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.00, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.00, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.00, 5.0));
+        System.out.println("10,000 at 6% interest = " + calculateInterest(10000.00, 6.0));
+
+        // using a for loop to do the work for us
+        // for (iterator; condition; increment / decrement)
+        for(int i = 0; i < 5; i++){
+            System.out.println("'i' is currently " + i);
+        }
+
+        /*
+            using the for statement, call the calculateInterest method with
+            the amount of 10K with an interest rate of 2,3,4,5,6,7 and 8%
+            and print the results to the console.
+         */
+
+        for (double i = 1; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount * (interestRate/100));
+    }
 }
