9d9d1bbc0cba73c0a2d473a487bd2ed55b720e2f
kyle Guerrero <kyleguerrero85@gmail.com>
2020-05-28 14:17:43 -0500

    feat: added parsing strings
    the parsing strings dir demonstarates how we
    can parse pritimtive data types from a string
    and also displays use of primitive type wrapper
    classes

--- 02.ControlFlow/04.ParsingValuesFromAString/src/Main.java
+++ 02.ControlFlow/04.ParsingValuesFromAString/src/Main.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        // use parsing methods to parse a string to get a primitive type out of a string.
+        String numberAsString = "2020";
+        System.out.println("Our string is: " + numberAsString);
+
+        // Using the int wrapper class, we attempt to parse out an integer
+        // if this fails it will throw out an error;
+        int numberFromString = Integer.parseInt(numberAsString);
+        System.out.println("The number generated from parseInt: " + numberFromString);
+
+        // When adding strings to numbers, Java will type coerce the int to a String and then
+        // concatenate the two strings together.
+        // usually this is not what you want, you will want to  convert the string to a number, and
+        // then add up two values together
+
+        numberAsString += 1;
+        System.out.println("Our string number is now: " + numberAsString);
+
+        numberFromString += 1;
+        System.out.println("Our number FROM A STRING is now: " + numberFromString);
+    }
 }
